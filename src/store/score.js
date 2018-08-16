a5f77f7a7ed298955dbbe143e50c11f12738ed8f
huojinzhao <352782712@qq.com>
2018-08-16 16:41:04 +0800

    refactor(store): 更新数据结构

--- src/store/score.js
+++ src/store/score.js
@@ -5,52 +5,40 @@
  */
 
 export default {
-  data: () => ({
-    // 学生积分统计
-    statistic: {
-      // 总分统计
-      total_score_rank: [],
-      // 类型游戏总分统计
-      game_type_score_rank: [],
-      // 单个游戏分数统计
-      game_score_rank: [],
-    },
-  }),
-
-  computed: {
-    // game环节总排名 learn环节
-    practiceScoreList() {
-      return this.state.section === 'game'
-        ? this.statistic.game_type_score_rank
-        : this.statistic.game_score_rank;
-    },
+  data() {
+    return { score: this.scoreInit() };
   },
 
   created() {
-    // 后端推送学生积分更新
-    this.$ws.on('game_score_rank', this.updateStatistic);
+    // 后端推送积分数据
+    this.$ws.on('game_score_rank', this.scoreUpdate);
 
-    // 前端更新学生统计数据
-    this.$on('updateStatistic', this.updateStatistic);
+    // 前端更新积分数据
+    this.$on('scoreUpdate', this.scoreUpdate);
   },
 
   methods: {
-    // 更新积分统计
-    updateStatistic({ data }) {
-      const statistic = data ? JSON.parse(data) : null;
+    // 初始化积分数据
+    scoreInit() {
+      return {
+        // 练习类型
+        game_type: '',
+        // 练习名称
+        game_name: '',
+        // 总分统计
+        total_score_rank: [],
+        // 类型游戏总分统计
+        game_type_score_rank: [],
+        // 单个游戏分数统计
+        game_score_rank: [],
+      };
+    },
 
-      this.statistic = statistic
-        ?
-        {
-          ...this.statistic,
-          ...statistic,
-        }
-        :
-        {
-          ...this.statistic,
-          game_score_rank: [],
-          game_type_score_rank: [],
-        };
+    // 更新积分数据
+    scoreUpdate({ data }) {
+      const score = data ? JSON.parse(data) : null;
+
+      this.score = score || this.scoreInit();
     },
   },
 };
