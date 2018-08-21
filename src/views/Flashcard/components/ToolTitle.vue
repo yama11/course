62bce6a5190228c5928fea260f21c3e24d4da08a
suyanping <344057827@qq.com>
2018-08-21 09:42:37 +0800

    feat(ToolTitle): 标题提示语

---
+++ src/views/Flashcard/components/ToolTitle.vue
@@ -0,0 +1,47 @@
+<script>
+/**
+ * @overview 文本提示
+ *
+ * @author suyanping
+ */
+
+export default {
+  name: 'ToolTitle',
+
+  props: {
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+
+  data() {
+    return {
+
+    };
+  },
+
+  methods: {
+
+  },
+
+};
+</script>
+
+<template>
+  <div
+    class="tool-title">
+    {{ title }}
+  </div>
+</template>
+
+<style lang="postcss">
+.tool-title{
+  position: absolute;
+  background: white;
+  padding: 20px 100px;
+  font-size: 45px;
+  border-radius: 20px;
+  left: 750px;
+}
+</style>
