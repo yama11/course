83f08931b920aabb0552b93cb99b91bb0e600beb
huojinzhao <352782712@qq.com>
2018-10-10 09:46:56 +0800

    refactor(Practice/Loading)

--- src/views/Practice/components/Loading.vue
+++ src/views/Practice/components/Loading.vue
@@ -7,17 +7,84 @@
 
 export default {
   name: 'PracticeLoading',
+
+  props: {
+    scene: {
+      type: String,
+      default: '',
+    },
+
+    bar: {
+      type: String,
+      default: '',
+    },
+
+    time: {
+      type: Number,
+      default: 1,
+    },
+  },
+
+  computed: {
+    cssRoot() {
+      return {
+        'background-image': `url(${this.scene})`,
+      };
+    },
+
+    cssProgress() {
+      return {
+        'background-image': `url(${this.bar})`,
+        'animation-duration': `${this.time}s`,
+      };
+    },
+  },
 };
 </script>
 
 <template>
-  <div class="practice-loading">
-    练习加载提示组件
+  <div
+    :style="cssRoot"
+    class="
+      global-scene
+      global-ground
+      global-backdrop
+      practice-loading
+    "
+  >
+    <div
+      :style="cssProgress"
+      class="
+        global-backdrop
+        practice-loading__progress
+      "
+      @animationend="emit('end')"
+    />
   </div>
 </template>
 
 <style lang="postcss">
 .practice-loading {
+  position: fixed;
+  display: flex;
+  justify-content: center;
+  padding-left: 74px;
+  z-index: 100;
+}
 
+.practice-loading__progress {
+  position: absolute;
+  border-radius: 18px;
+  height: 36px;
+  width: 824px;
+  bottom: 70px;
+  animation-name: practiceLoadingProgress;
+  animation-timing-function: ease-in-out;
+}
+
+@keyframes practiceLoadingProgress {
+  from {
+    background-position: -824px;
+  }
 }
 </style>
--- src/views/index.js
+++ src/views/index.js
@@ -2,5 +2,6 @@ export { default as ViewBoot } from './Boot';
 export { default as ViewCelebrate } from './Celebrate';
 export { default as ViewConcert } from './Concert';
 export { default as ViewFlashcard } from './Flashcard';
+export { default as ViewPractice } from './Practice';
 export { default as ViewTeaching } from './Teaching';
 export { default as ViewTelevision } from './Television';
