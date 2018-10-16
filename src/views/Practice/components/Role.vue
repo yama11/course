e0be66b0e2712d7c3616a16e0f6a36bc40832bd9
suyanping <344057827@qq.com>
2018-10-16 17:08:15 +0800

    feat(Role): 人物展示

---
+++ src/views/Practice/components/Role.vue
@@ -0,0 +1,89 @@
+<script>
+/**
+ * @overview 人物
+ *
+ * @author suyanping
+ */
+
+export default {
+  name: 'PracticeRole',
+
+  props: {
+    // 名字
+    name: {
+      type: String,
+      default: '',
+    },
+
+    // 头像url
+    imgUrl: {
+      type: String,
+      default: '',
+    },
+
+    // 是否选中正确答案
+    selectResult: {
+      type: Boolean,
+      default: false,
+    },
+
+    // 字体颜色
+    textColor: {
+      type: String,
+      default: 'red',
+    },
+  },
+
+  data() {
+    return {
+
+    };
+  },
+
+  computed: {
+    styleColor() {
+      return { color: this.textColor };
+    },
+  },
+
+  methods: {
+
+  },
+};
+</script>
+
+
+<template>
+  <div class="practice-role">
+
+    <span
+      class="practice-role__text">{{ name }}</span>
+
+    <span
+      v-if="selectResult"
+      :style="styleColor"
+      class="practice-role__score">
+      +1
+    </span>
+
+    <img
+      :src="imgUrl">
+  </div>
+</template>
+
+
+<style lang="postcss">
+.practice-role{
+  display: inline-block；
+}
+
+.practice-role__text{
+  color: white;
+}
+
+.practice-role__score{
+  position: absolute;
+  top: 15px;
+  right: 2px;
+}
+</style>
