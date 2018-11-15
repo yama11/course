7594e0bd963df2a9887970b34f38992177b52865
yehaifeng <dreams_ye@163.com>
2018-11-15 11:03:37 +0800

    feat :教学场景四

---
+++ src/views/Teaching/ModeFour/Background.vue
@@ -0,0 +1,25 @@
+<script>
+/**
+ * @overview 教学场景四 - 多图多文后景
+ *
+ * @author yehaifeng
+ */
+
+export default {
+  name: 'LearnBackgroundModeFour',
+
+  props: {
+
+  },
+};
+</script>
+
+<template>
+  <div class="teaching-background global-scene"/>
+</template>
+
+<style lang="postcss" scoped>
+.teaching-background {
+
+}
+</style>
---
+++ src/views/Teaching/ModeFour/Foreground.vue
@@ -0,0 +1,62 @@
+<script>
+/**
+ * @overview  教学场景四 - 多图多文前景
+ *
+ * @author yehaifeng
+ */
+
+export default {
+  name: 'LearnForegroundModeFour',
+
+  props: {
+    src: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  data: () => ({
+    director: {
+      disabled: true,
+    },
+  }),
+
+  computed: {
+    animations() {
+      return this.$animate.settle();
+    },
+  },
+
+  created() {
+    this.$audio.play();
+  },
+};
+</script>
+
+<template>
+  <div
+    class="
+      global-scene
+      teaching-foreground
+    "
+  >
+    <AppBackgroundCard
+      v-for="(item, index) in src.cards"
+      :class="animations[index]"
+      :key="item.url"
+      :card="item.url"
+      :text="item.text"
+      :is-transparent="true"
+    />
+    <AppDirector :disabled="director.disabled" />
+  </div>
+</template>
+
+<style lang="postcss" scoped>
+.teaching-foreground {
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  flex-wrap: wrap;
+}
+</style>
